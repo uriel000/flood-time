742f718340bd3b4f53c19c583c098484290d9aae
uriel000 Github <jesrielledesma@gmail.com>
2023-07-28 19:11:43 +0800

    Cleaned some codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,25 +13,25 @@ const mintemperature = document.querySelector(
   ".home-container .weather-data .min-temperature .temp"
 );
 
-// fetch(
-//   "https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=dc68862ea6931926eadd7ba3744280cd"
-// )
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     const location = data["name"];
-//     const tempeValue = data["main"]["temp"] - 273.15;
-//     const descValue = data["weather"][0]["description"];
-//     const maxTemp = data["main"]["temp_max"] - 273.15;
-//     const minTemp = data["main"]["temp_max"] - 273.15;
-//     temperature.innerHTML = Math.round(tempeValue);
-//     maxtemperature.innerHTML = Math.round(maxTemp) + "°C";
-//     mintemperature.innerHTML = Math.round(minTemp) + "°C";
-//     description.innerHTML = descValue;
-//   })
-//   .catch((err) => console.log(err));
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=dc68862ea6931926eadd7ba3744280cd"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const location = data["name"];
+    const tempeValue = data["main"]["temp"] - 273.15;
+    const descValue = data["weather"][0]["description"];
+    const maxTemp = data["main"]["temp_max"] - 273.15;
+    const minTemp = data["main"]["temp_max"] - 273.15;
+    temperature.innerHTML = Math.round(tempeValue);
+    maxtemperature.innerHTML = Math.round(maxTemp) + "°C";
+    mintemperature.innerHTML = Math.round(minTemp) + "°C";
+    description.innerHTML = descValue;
+  })
+  .catch((err) => console.log(err));
 
-temperature.innerHTML = 26;
-maxtemperature.innerHTML = 27 + "°C";
-mintemperature.innerHTML = 24 + "°C";
-description.innerHTML = "Mist";
+// temperature.innerHTML = 26;
+// maxtemperature.innerHTML = 27 + "°C";
+// mintemperature.innerHTML = 24 + "°C";
+// description.innerHTML = "Mist";
